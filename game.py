ae9bcde1b8bbf4af46dae1ac85191886b60efdb4
Emma Braddock <emmabraddock1604@gmail.com>
2025-10-15 12:13:54 -0600

    adding game.py to my github repository

---
+++ game.py
@@ -0,0 +1,13 @@
+#Emma Braddock
+#10/13/25
+#CSCI 150
+
+"""Imports gamefunctions.py and uses those functions to create a game.
+
+
+
+"""
+
+from gamefunctions import *
+
+print(new_random_monster())
